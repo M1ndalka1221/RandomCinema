1bf5c31351f4566ad8073a0f96a3f69c68253711
EugeneHard-cmd <eugenehard69@gmail.com>
2026-05-27 22:48:29 +0200

    Made some changes to app.py file. Added template and data

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import json
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def main_page():
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run()
